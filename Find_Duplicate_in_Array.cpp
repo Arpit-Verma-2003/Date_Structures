65b69c7d16e472bb793effedb906759cb28acd03
Arpit Verma <arpitverma200316@gmail.com>
2023-10-30 18:58:48 +0530

    Create Find_Duplicate_in_Array.cpp

---
+++ Find_Duplicate_in_Array.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int findDuplicate(vector<int> &arr, int n){
+	// Write your code here.
+	// tortoise method of circular linked list
+	int slow = arr[0];
+	int fast = arr[0];
+        do {
+			slow= arr[slow];
+			fast= arr[arr[fast]];
+        }while (slow != fast);
+		fast = arr[0];
+		while(slow!=fast){
+			slow = arr[slow];
+			fast = arr[fast];
+		}
+		return slow;
