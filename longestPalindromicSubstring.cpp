d5e2bd721f9b0d75ada71d9a3bddac31f034d409
Arpit Verma <arpitverma200316@gmail.com>
2024-01-01 23:48:39 +0530

    Create longestPalindromicSubstring.cpp

---
+++ longestPalindromicSubstring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int left = 0;
+        int right = s.length()-1;
+        string temp = "";
+        string ans = "";
+        while(left<=right){
+            if(s[left]!=' '){
+                temp+=s[left];
+            }
+            else if(s[left]==' '&&temp!=""){
+                if (ans!="") ans = temp + " " + ans;
+                else ans = temp;
+                temp = "";
+            }
+            left++;
+        }	
+        if(temp!=""){
+            if (ans!="") ans = temp + " " + ans;
+            else ans = temp;
+        }
+        return ans;
+    }
+};
