57e3b757b1d1b25cb77895a4c880210bc32bded9
Arpit Verma <arpitverma200316@gmail.com>
2024-01-24 01:09:24 +0530

    Create bipartiteGraphDFS.cpp

---
+++ bipartiteGraphDFS.cpp
@@ -0,0 +1,25 @@
+ bool dfs(int node,int col,int color[],vector<int>adj[]){
+        color[node]=col;
+        for(auto it:adj[node]){
+            if(color[it]==-1){
+                if(!(dfs(it,!col,color,adj))) return false;
+            }
+            else if(color[it]==col) return false;
+        }
+        return true;
+    }
+public:
+	bool isBipartite(int V, vector<int>adj[]){
+	    // Code here
+	   int color[V];
+	   for(int i=0;i<V;i++) color[i]=-1;
+	   for(int i=0;i<V;i++){
+	       if(color[i]==-1){
+	           //if(!checkBipartite(i,V,adj,color)) return false; // bfs
+	           if(!dfs(i,0,color,adj)) return false;
+	       }
+	   }
+	   return true;
+	}
+
+};
